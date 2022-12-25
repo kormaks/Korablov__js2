92126b5ee3c6003c3c199c95fcd1b3edc489ba4b
Maks <mkorablov30@gmail.com>
2022-12-25 16:55:04 +0200

    create variables and try working with console

---
+++ script.js
@@ -0,0 +1,15 @@
+const userAge = 21;
+const userName = "Maksim";
+const userLastName = "Korblov";
+
+const newUser = "Simon";
+const shopingList = ["Milk", "Coffee", "Bread"];
+const activeUser = true;
+const employeeSalery = 30000;
+const mouseCoordinates = {
+    x : 10,
+    y : -15
+};
+
+console.log("Hello World!");
+console.log("userAge", userAge);
